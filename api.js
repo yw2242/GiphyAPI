e6055e08bd001f2c39f3578f80a3984f6e1d760b
Megan Wu <meganwu1994@gmail.com>
2019-06-09 23:57:37 -0500

    added some js and .ajax

--- api.js
+++ api.js
@@ -1,21 +1,38 @@
-// create an array for the categories
+$(document).ready(function() {
+
+    // create an array for the categories
+    var animals = ["Cat", "Bird", "Dog", "Fish", "Rabbit", "Monkey", "Panda", "Lion", "Bear", "Crab"];
+
+    function renderButton(animals) {
+        var $btn = $("<button>");
+            .addClass("animalBtn")
+            .attr("data-name", animals);
+            .text(animals);
+
+        $("header").append($btn);
+    }
 
 
 
-// api key
-var queryURL = "" + movie + "AhUbAzJjmfaOu1O0URVc7zrzPrzncMo3";
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    $("#movie-view").text(JSON.stringify(response));
-  });
 
 
+    // generic function for capturing the movie name from the data-attribute
 
-// generic function for capturing the movie name from the data-attribute
+    $(document).on("click", ".animalBtn", function() {
+        // api key
+        var queryURL = "" + animals + "AhUbAzJjmfaOu1O0URVc7zrzPrzncMo3";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            $("#giphy").text(JSON.stringify(response));
+        });
+    })
+
+
+    // Here we grab the text from the input box
 
 
 
-// Here we grab the text from the input box
+}
